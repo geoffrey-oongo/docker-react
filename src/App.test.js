dddbf6d5312d9f29d147f860c8b108bce62a3857
Geoffrey Oongo <goongo@Aealimited.com>
2022-02-14 07:24:27 +0300

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -5,4 +5,6 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  const linkElementt = screen.getByText(/learn react/i);
+  expect(linkElementt).toBeInTheDocument();
 });
